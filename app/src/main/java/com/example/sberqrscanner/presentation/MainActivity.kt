121cc66910e52175ebc56a796278c3ec3e62cb51
Gebort <35885530+Gebort@users.noreply.github.com>
2022-06-23 09:11:34 +1000

    Updated bottomsheet

--- app/src/main/java/com/example/sberqrscanner/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sberqrscanner/presentation/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.sberqrscanner.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.Window
+import android.view.WindowManager
 import com.example.sberqrscanner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+        )
+
         setContentView(R.layout.activity_main)
     }
 
