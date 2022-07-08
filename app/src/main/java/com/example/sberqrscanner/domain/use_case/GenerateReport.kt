f89c5b4a577f05cd372f3b2fc48f410d3307db93
Gebort <35885530+Gebort@users.noreply.github.com>
2022-07-08 14:04:16 +1000

    Multiple pages in report

--- app/src/main/java/com/example/sberqrscanner/domain/use_case/GenerateReport.kt
+++ app/src/main/java/com/example/sberqrscanner/domain/use_case/GenerateReport.kt
@@ -14,28 +14,38 @@ import com.example.sberqrscanner.domain.model.Division
 
 private const val PAGE_HEIGHT = 1120
 private const val PAGE_WIDTH = 792
+private const val PADDING = 30F
 
 class GenerateReport {
 
     operator fun invoke(divisions: List<Division>, profile: Profile, context: Context): PdfDocument {
-        val document = PdfDocument()
-        val pageInfo = PageInfo.Builder(PAGE_WIDTH, PAGE_HEIGHT, 1).create()
-        val page: PdfDocument.Page = document.startPage(pageInfo)
 
         val title = Paint()
         title.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
         title.color = Color.BLACK
 
-        val canvas = page.canvas
+        val writer = DocWriter()
 
         title.textAlign = Paint.Align.CENTER
         title.textSize = 24F
         val date = getCurrentDate().toString(context.resources.getString(R.string.date_format))
         val dateStr = context.resources.getString(R.string.report, date)
-        canvas.drawText(dateStr, PAGE_WIDTH/2F,30F , title)
+       // canvas.drawText(dateStr, PAGE_WIDTH/2F,30F , title)
+        writer.writeInDoc(
+            PAGE_WIDTH/2F,
+            30F,
+            dateStr,
+            title
+        )
 
         title.textSize = 20F
-        canvas.drawText("${profile.city}, ${profile.address}", PAGE_WIDTH/2F,60F , title)
+       // canvas.drawText("${profile.city}, ${profile.address}", PAGE_WIDTH/2F,60F , title)
+        writer.writeInDoc(
+            PAGE_WIDTH/2F,
+            30F,
+            "${profile.city}, ${profile.address}",
+            title
+        )
 
         title.textAlign = Paint.Align.LEFT
         title.textSize = 15F
@@ -48,36 +58,91 @@ class GenerateReport {
         val absentListStr = context.resources.getString(R.string.report_absent_list)
 
 
-        canvas.drawText(absentStr, 30F, 100F, title)
-        canvas.drawText(totalStr, 30F, 120F, title)
-        canvas.drawText(absentListStr, 30F, 160F, title)
+     //   canvas.drawText(absentStr, 30F, 100F, title)
+        writer.writeInDoc(
+            30F,
+            40F,
+            absentStr,
+            title
+        )
+      //  canvas.drawText(totalStr, 30F, 120F, title)
+        writer.writeInDoc(
+            30F,
+            20F,
+            totalStr,
+            title
+        )
+ //       canvas.drawText(absentListStr, 30F, 160F, title)
+        writer.writeInDoc(
+            30F,
+            40F,
+            absentListStr,
+            title
+        )
 
         for (i in 1..absentCount) {
-            if (160F+(i*20F)+40 < PAGE_HEIGHT) {
-                canvas.drawText(
-                    "$i. ${divisions[i - 1].name}",
+            writer.writeInDoc(
                     30F,
-                    160F + (i * 20F),
+                    20F,
+                    "$i. ${divisions[i - 1].name}",
                     title
                 )
-            }
-            else {
-                val moreStr = context.resources.getString(
-                    R.string.report_absent_more,
-                    absentCount-i+1
-                )
-                canvas.drawText(
-                    moreStr,
-                    60F,
-                    160F + (i * 20F),
-                    title
-                )
-                break
-            }
         }
 
-        document.finishPage(page)
 
-        return document
+
+        return writer.getDoc()
+    }
+
+    private inner class DocWriter {
+        private val pages = mutableListOf<PdfDocument.Page>()
+        private val doc = PdfDocument()
+        var lastY = PADDING
+
+        init {
+            newPage()
+        }
+
+        private fun newPage(){
+            if (pages.isNotEmpty()){
+                doc.finishPage(pages.last())
+            }
+            val pageInfo = PageInfo.Builder(PAGE_WIDTH, PAGE_HEIGHT, pages.size).create()
+            val newPage: PdfDocument.Page = doc.startPage(pageInfo)
+            pages.add(newPage)
+        }
+
+        fun writeInDoc(
+            x: Float,
+            dy: Float,
+            text: String,
+            paint: Paint,
+        ){
+            lastY = if (lastY + dy + PADDING > PAGE_HEIGHT){
+                newPage()
+                PADDING
+            } else {
+                (lastY + dy)
+            }
+
+            if (lastY < PADDING) {
+                lastY = PADDING
+            }
+
+            val page = pages.last()
+            page.canvas.drawText(
+                text,
+                x % PAGE_WIDTH,
+                lastY,
+                paint
+            )
+
+        }
+
+        fun getDoc(): PdfDocument {
+            doc.finishPage(pages.last())
+            return doc
+        }
+
     }
 }
